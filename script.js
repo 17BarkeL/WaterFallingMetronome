371f8cad1b771cc872c76b871bf15d7aa060bb2f
Luke Barkess <luke.barkess@icloud.com>
2023-01-16 11:19:55 +0000

    es

--- script.js
+++ script.js
@@ -9,14 +9,17 @@ var BPMMax = parseInt(BPMSlider.getAttribute("max"))
 
 BPMSlider.addEventListener("change", () => {
     BPMInput.value = BPMSlider.value;
+    updateTick();
 })
 
 BPMIncrease.addEventListener("click", () => {
     changeBPM(Math.min(parseInt(BPMInput.value) + BPMStep, BPMMax));
+    updateTick();
 });
 
 BPMDecrease.addEventListener("click", () => {
     changeBPM(Math.max(parseInt(BPMInput.value) - BPMStep, BPMMin));
+    updateTick();
 });
 
 BPMInput.addEventListener("change", () => {
@@ -27,11 +30,69 @@ BPMInput.addEventListener("change", () => {
     else {
         changeBPM(60);
     }
+
+    updateTick();
 });
-
-
 
 function changeBPM(BPM) {
     BPMInput.value = BPM;
     BPMSlider.value = BPM;
+}
+
+function updateTick() {
+    if (playing) {
+        stopTick();
+        playTick();
+    }
+}
+
+function getBPM() {
+    return BPMInput.value;
+}
+
+
+
+
+
+
+
+
+
+
+
+var tick = new Audio("N:\Downloads\tick.mp3");
+var tickInterval;
+var toggleButton = document.querySelector("#toggle");
+var mainElement = document.querySelector("main");
+var playing = false;
+
+toggleButton.addEventListener("click", () => {
+    if (playing) {
+        stopTick();
+
+        toggleButton.innerText = "Start";
+        playing = false;
+    }
+
+    else {
+        playTick();
+
+        toggleButton.innerText = "Stop";
+        playing = true;
+    }
+});
+
+function playTick() {
+    tickInterval = setInterval(() => {
+        console.log("tick");
+        tick.play();
+        mainElement.style.backgroundColor = "cyan";
+        setTimeout(() => {
+            mainElement.removeAttribute("style");
+        }, 100);
+    }, 1 / (getBPM() / 60) * 1000);
+}
+
+function stopTick() {
+    clearInterval(tickInterval);
 }
